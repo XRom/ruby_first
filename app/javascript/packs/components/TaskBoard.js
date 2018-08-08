8aa0130b9964cba18e2e733884f3a5335f24f434
Roman Kharitov <xrom.raiders@gmail.com>
2018-08-09 03:31:39 +0400

    try use real data

--- app/javascript/packs/components/TaskBoard.js
+++ app/javascript/packs/components/TaskBoard.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import Board from 'react-trello'
+//import {fetch} from 'whatwg-fetch'
 
 const data = {
   lanes: [
@@ -22,7 +23,40 @@ const data = {
 }
 
 export default class TaskBoard extends React.Component {
+
+  setState(value) {
+    this.state = value;
+    console.log('setState', value);
+  }
+
+  constructor(props) {
+      super(props);
+
+      this.state = {
+        tasks: [],
+      };
+  }
+  
+  componentDidMount() {
+    console.log('componentDidMount')    
+
+    fetch('http://localhost:3000/api/v1/tasks.json')
+      .then(function(response) {
+        return response.json()
+      }).then(function(json) {
+        const jj = json['items']
+        console.log('parsed json', jj)
+        this.setState(jj);
+      }).catch(function(ex) {
+        console.log('parsing failed', ex)
+      });
+  }
+
   render() {
+//    const { tasks } = this.state;
+   console.log('renddder', this.state)
+//    return <Board data={tasks} />
+
     return <Board data={data} />
   }
 }
